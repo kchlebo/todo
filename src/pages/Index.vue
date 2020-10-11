f1fd5b91404b071b471639b1c21a131df7e75c45
kornel.chlebovics@agpulse.com <kornel.chlebovics@agpulse.com>
2020-10-11 15:13:24 +0300

    sanity logic for vue

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,14 +1,29 @@
 <template>
-  <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-full.svg"
-    >
+  <q-page padding>
+    <input v-model="message" @keyup.esc="clearMessage"
+    @keyup.enter="alertMessage" >
+    <button @click="clearMessage">Clear</button>
+    <h5>{{ message }}</h5>
   </q-page>
 </template>
 
 <script>
 export default {
-  name: 'PageIndex'
+  data() {
+    return {
+      message: "I love vue.js soso hard!"
+    }
+  },
+  methods: {
+    clearMessage() {
+      this.message = ''
+    },
+    alertMessage() {
+      alert(this.message)
+    }
+  }
 }
 </script>
+<style>
+
+</style>
